3d44b5237ca33a0df4111e07fc38431369842635
Shantanu Jana <71997730+Arceus-sj@users.noreply.github.com>
2022-01-28 20:58:16 +0530

    Create variable.py

---
+++ Fundamentals/variable.py
@@ -0,0 +1,35 @@
+name = "Cypher Jana" # name is of type str
+age = 19             # age is of type int
+haveUID = False      # haveUID is of type Bool
+
+print(name)
+
+# Casting
+
+x = int(3)
+y = str(3)
+z = float(3)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Assign multiple values
+colour1, colour2, colour3 = "Red", "Green", "Blue"
+print(colour1)
+print(colour2)
+print(colour3)
+
+# Assign one value to multiple variable
+
+a = b = c = 100
+print(a)
+print(b)
+print(c)
+
+# Example
+rent = 1500
+item1 = "Rent"
+item2 = "Gas"
+item3 = "Groceries"
+print("Expense items:",item1,item2,item3)
